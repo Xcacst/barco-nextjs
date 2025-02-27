981d02dc5cfbc79b91c3dd84b0bd205435c27640
Yannick <yannick.liebnau@gmail.com>
2025-02-27 23:41:23 +0100

    Test new music echoesinabottle02

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -49,7 +49,7 @@ export default function MusicPlayer({play}) {
 	return (
 		 <>
 			 <div className={"playBtnContainer"} style={{...visibility}}>
-				 <audio ref={audioRef} src="/echoesinabottle.mp3" loop/>
+				 <audio ref={audioRef} src="/echoesinabottle02.mp3" loop/>
 				 <div className={isPlaying ? "playBtn play" : "playBtn pause"} onClick={handlePlayPause}>
 					 {Array.from({length: 10}).map((_, i) => (
 							<span key={i} style={{}}></span>
